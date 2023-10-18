cfb28de3fb237ee9096e2076cdac5b52ddba8a79
zjw57 <zjw57@qq.com>
2023-10-18 19:20:46 +0800

    MT7621 frp fixed

--- devices/ramips_mt7621/diy.sh
+++ devices/ramips_mt7621/diy.sh
@@ -4,5 +4,4 @@
 sed -i 's/192.168.1/172.16.1/g' package/base-files/files/bin/config_generate
 
 # frp
-sed -i 's/0.51.3/0.52.1/g' package/feeds/packages/frp/Makefile
-sed -i 's/83032399773901348c660d41c967530e794ab58172ccd070db89d5e50d915fef/skip/g' package/feeds/packages/frp/Makefile
+sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' package/feeds/packages/frp/Makefile

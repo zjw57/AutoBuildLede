2c5a296957360ede19efc95d33aa9449a8e4a57e
zjw57 <zjw57@qq.com>
2023-10-20 23:15:37 +0800

    MT7621 adguardhome fixed

--- devices/ramips_mt7621/diy.sh
+++ devices/ramips_mt7621/diy.sh
@@ -5,3 +5,6 @@ sed -i 's/192.168.1/172.16.1/g' package/base-files/files/bin/config_generate
 
 # frp
 sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' package/feeds/packages/frp/Makefile
+
+# adguardhome
+sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' package/feeds/packages/adguardhome/Makefile
